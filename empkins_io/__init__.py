7825745844908ddb532807cba22a7734df7de2c5
Robert Richer <robert.richer@fau.de>
2021-10-19 23:51:01 +0200

    moved sensor_validation into own package

--- empkins_io/__init__.py
+++ empkins_io/__init__.py
@@ -1,9 +1,8 @@
 import empkins_io.io  # noqa: F401
 import empkins_io.sensors  # noqa: F401
 import empkins_io.signal_alignment  # noqa: F401
-import empkins_io.sensor_validation  # noqa: F401
 
-__all__ = ["io", "sensors", "signal_alignment", "sensor_validation"]
+__all__ = ["io", "sensors", "signal_alignment"]
 
 
 __version__ = "0.1.0"
