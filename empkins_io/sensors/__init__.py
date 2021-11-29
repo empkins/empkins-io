d7fc5964c4cbe6f17a1d7952b97e34ef1634a2be
Robert Richer <robert.richer@fau.de>
2021-11-29 10:22:33 +0100

    added eo_mbs import

--- empkins_io/sensors/__init__.py
+++ empkins_io/sensors/__init__.py
@@ -1,5 +1,6 @@
 from empkins_io.sensors.mis import mis
+from empkins_io.sensors.eo_mbs import eo_mbs
 import empkins_io.sensors.motion_capture
 import empkins_io.sensors.utils
 
-__all__ = ["mis", "motion_capture", "utils"]
+__all__ = ["mis", "eo_mbs", "motion_capture", "utils"]
---
+++ empkins_io/sensors/eo_mbs/eo_mbs.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from biopsykit.utils._datatype_validation_helper import _assert_file_extension
+
+from empkins_io.utils._types import path_t
+
+
+def load_data(file_path: path_t) -> pd.DataFrame:
+    # ensure pathlib
+    file_path = Path(file_path)
+    _assert_file_extension(file_path, ".npz")
+    file = np.load(file_path.name, allow_pickle=True)  # load npz file
+    data = file["data"]
+    t = data[:, 0, :].ravel()  # time base
+    ya = data[:, 1, :].ravel()  # channel 1 (diffuse reflection)
+    yb = data[:, 2, :].ravel()  # channel 2 (specular reflection)
+
+    return pd.DataFrame({"ch_a": ya, "ch_b": yb}, index=pd.Index(t, name="time"))
