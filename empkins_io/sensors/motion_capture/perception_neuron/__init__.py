e21dbbf5e2f4e8b114cade6131df929d0e34b45d
Robert Richer <robert.richer@fau.de>
2021-12-04 00:32:05 +0100

    removed load_*_file functions (now all data loaded via load_perception_neuron_folder)

--- empkins_io/sensors/motion_capture/perception_neuron/__init__.py
+++ empkins_io/sensors/motion_capture/perception_neuron/__init__.py
@@ -1,8 +1,3 @@
-from empkins_io.sensors.motion_capture.perception_neuron.perception_neuron import (
-    load_bvh_file,
-    load_calc_file,
-    load_center_mass_file,
-    load_perception_neuron_folder,
-)
+from empkins_io.sensors.motion_capture.perception_neuron.perception_neuron import load_perception_neuron_folder
 
-__all__ = ["load_bvh_file", "load_center_mass_file", "load_calc_file", "load_perception_neuron_folder"]
+__all__ = ["load_perception_neuron_folder"]
