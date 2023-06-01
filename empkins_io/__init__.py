9c01a5e44224b20f47b93390bee68834d288edda
Luca Abel <luca.abel@fau.de>
2023-06-01 12:40:20 +0200

    datasets missing in init

--- empkins_io/__init__.py
+++ empkins_io/__init__.py
@@ -2,8 +2,9 @@ import empkins_io.processing  # noqa: F401
 import empkins_io.sensors  # noqa: F401
 import empkins_io.signal_alignment  # noqa: F401
 import empkins_io.utils  # noqa: F401
+import empkins_io.datasets # noqa: F401
 
-__all__ = ["sensors", "processing", "signal_alignment", "utils"]
+__all__ = ["sensors", "processing", "signal_alignment", "utils", "datasets"]
 
 
 __version__ = "0.1.0"
