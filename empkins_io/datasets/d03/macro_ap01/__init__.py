2dbdd87ee57a07a974df31f75a2c4046c92ee9cd
livhe <liv.herzer@gmx.de>
2023-07-19 14:06:00 +0200

    add MacroStudyTsstDatasetPerPhase to init imports

--- empkins_io/datasets/d03/macro_ap01/__init__.py
+++ empkins_io/datasets/d03/macro_ap01/__init__.py
@@ -1,5 +1,6 @@
 from empkins_io.datasets.d03.macro_ap01._base_dataset import MacroBaseDataset
 from empkins_io.datasets.d03.macro_ap01._gait_dataset import MacroStudyGaitDataset
 from empkins_io.datasets.d03.macro_ap01._tsst_dataset import MacroStudyTsstDataset
+from empkins_io.datasets.d03.macro_ap01._tsst_dataset_per_phase import MacroStudyTsstDatasetPerPhase
 
-__all__ = ["MacroBaseDataset", "MacroStudyGaitDataset", "MacroStudyTsstDataset"]
+__all__ = ["MacroBaseDataset", "MacroStudyGaitDataset", "MacroStudyTsstDataset", "MacroStudyTsstDatasetPerPhase"]
