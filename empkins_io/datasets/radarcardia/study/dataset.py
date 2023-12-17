5f65aa8575cde0a4c08109c591b4b011620c1a2d
moestenfau <marie.oesten@fau.de>
2023-12-17 12:01:52 +0100

    add excluding of measurements with corrupted ecgs

--- empkins_io/datasets/radarcardia/study/dataset.py
+++ empkins_io/datasets/radarcardia/study/dataset.py
@@ -16,6 +16,7 @@ from empkins_io.utils._types import path_t
 
 class RadarCardiaStudyDataset(BaseDataset):
     exclude_ecg_seg_failed: bool
+    exclude_ecg_corrupted: bool
 
     BIOPAC_CHANNEL_MAPPING: Dict[str, str] = {
         "ecg": "ecg",
@@ -27,6 +28,9 @@ class RadarCardiaStudyDataset(BaseDataset):
 
     SUBJECTS_ECG_SEG_FAILED: Tuple[str] = ("VP_04",)
 
+    SUBJECT_ECG_CORRUPTED: Tuple[Tuple] = (("VP_17", "baseline", "normal"),
+                                           ("VP_17", "temporalis", "normal"),)
+
     def __init__(
             self,
             base_path: path_t,
@@ -35,9 +39,11 @@ class RadarCardiaStudyDataset(BaseDataset):
             use_cache: Optional[bool] = False,
             calc_biopac_timelog_shift: Optional[bool] = True,
             trigger_data_extraction: Optional[bool] = False,
-            exclude_ecg_seg_failed: Optional[bool] = True
+            exclude_ecg_seg_failed: Optional[bool] = True,
+            exclude_ecg_corrupted: Optional[bool] = True
     ):
         self.exclude_ecg_seg_failed = exclude_ecg_seg_failed
+        self.exclude_ecg_corrupted = exclude_ecg_corrupted
 
         super().__init__(
             base_path=base_path,
@@ -66,6 +72,12 @@ class RadarCardiaStudyDataset(BaseDataset):
         index = list(product(body_parts_1, ["normal"]))
         index.extend(list(product(body_parts_2, breathing)))
         index = [(subject, *i) for subject, i in product(subject_ids, index)]
+
+        if self.exclude_ecg_corrupted:
+            for subject_id, body_part, breathing in self.SUBJECT_ECG_CORRUPTED:
+                if (subject_id, body_part, breathing) in index:
+                    index.remove((subject_id, body_part, breathing))
+
         index = pd.DataFrame(index, columns=["subject", "location", "breathing"])
 
         return index
