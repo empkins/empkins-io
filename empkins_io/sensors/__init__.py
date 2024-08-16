047bdaa5a9d2488409f07f88ead32e167c5fef84
Marie Oesten <marie.oesten@fau.de>
2024-08-16 17:50:13 +0200

    add tfm dataset

--- empkins_io/sensors/__init__.py
+++ empkins_io/sensors/__init__.py
@@ -2,5 +2,6 @@ import empkins_io.sensors.motion_capture
 import empkins_io.sensors.utils
 from empkins_io.sensors.eo_mbs import eo_mbs
 from empkins_io.sensors.mis import mis
+from empkins_io.sensors.tfm import tfm
 
-__all__ = ["mis", "eo_mbs", "motion_capture", "utils"]
+__all__ = ["mis", "eo_mbs", "motion_capture", "utils", "tfm"]
