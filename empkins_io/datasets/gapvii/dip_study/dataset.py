2584c6403e29249d581e9aa585fb630b3f48f788
lukas-varga <lukas.varga128@gmail.com>
2024-09-16 17:40:00 +0200

    Added date property and loading date from protocol

--- empkins_io/datasets/gapvii/dip_study/dataset.py
+++ empkins_io/datasets/gapvii/dip_study/dataset.py
@@ -6,11 +6,12 @@ from tpcp import Dataset
 from pathlib import Path
 import numpy as np
 
+from empkins_io.sensors.tfm.tfm import TfmLoader
 from empkins_io.utils._types import path_t
 
 from biopsykit.utils.file_handling import get_subject_dirs
 
-from empkins_io.datasets.gapvii.dip_study.helper import _load_general_information
+from empkins_io.datasets.gapvii.dip_study.helper import _load_general_information, _update_dates, _load_single_date
 
 
 class DipStudyDataset(Dataset):
@@ -24,6 +25,8 @@ class DipStudyDataset(Dataset):
 
     SUBJECTS_MISSING: Tuple[str] = ("VP_15", "VP_18")
 
+    DEF_DATE = "01.01.1970"
+
     def __init__(
             self,
             base_path: path_t,
@@ -36,21 +39,43 @@ class DipStudyDataset(Dataset):
 
         super().__init__(groupby_cols=groupby_cols, subset_index=subset_index)
 
-    def create_index(self):
-
+    def _get_ids(self):
         subject_ids = [
             subject_dir.name for subject_dir in get_subject_dirs(self.base_path.joinpath("data_per_subject"), "VP_*")
         ]
-
         for subject_id in self.SUBJECTS_MISSING:
             if subject_id in subject_ids:
                 subject_ids.remove(subject_id)
+        return subject_ids
+
+    def create_index(self):
+        subject_ids = self._get_ids()
 
         phases = ["rest_1", "cpt", "rest_2", "straw", "rest_3"]
+        phases_mapping = {
+            "rest_1": "Ruhe 1",
+            "cpt": "CPT",
+            "rest_2": "Ruhe 2",
+            "straw": "Atmung",
+            "rest_3": "Ruhe 3"
+        }
 
         index = list(product(subject_ids, phases))
         index = pd.DataFrame(index, columns=["subject", "phase"])
         return index
+
+
+    def fill_dates(self):
+        subject_ids = self._get_ids()
+        subject_date_dict = {}
+
+        # Load dates for all subjects from protocol files
+        for subject in subject_ids:
+            date = _load_single_date(self.base_path, subject)
+            subject_date_dict[subject] = date
+
+        # Fill dates to empkins_dip_study.xlsx
+        _update_dates(self.base_path, subject_date_dict, "Sheet1")
 
     @property
     def sampling_rates(self) -> Dict[str, float]:
@@ -71,5 +96,26 @@ class DipStudyDataset(Dataset):
         return _load_general_information(base_path=self.base_path, column="cpt_duration")
 
     @property
+    def subject(self) -> str:
+        if self.is_single(["subject"]):
+            return self.index["subject"][0]
+        return None
+
+    @property
     def tfm_raw_data(self) -> Dict[str, Dict[str, np.ndarray]]:
-        pass
+        tfm_path = self.base_path.joinpath("")
+        tfm_loader = TfmLoader.from_mat_file(path=tfm_path)
+        return tfm_loader
+
+    @property
+    def date(self) -> str:
+        try:
+            if self.is_single(["subject"]):
+                return _load_general_information(base_path=self.base_path, column="date")[self.index["subject"][0]]
+
+            return _load_general_information(base_path=self.base_path, column="date")
+        except KeyError:
+            # Handle the KeyError, e.g., return a default value or log an error
+            print(f"Date was not found in empkins_dip_study.xlsx for subject {self.index['subject'][0]}")
+            print("Please check its protocol file and run fill_dates() method.")
+            return self.DEF_DATE  # or handle the error as needed
--- empkins_io/datasets/gapvii/dip_study/helper.py
+++ empkins_io/datasets/gapvii/dip_study/helper.py
@@ -1,4 +1,6 @@
 import datetime
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 from pathlib import Path
 from typing import Dict, Literal, Optional, Union
 from biopsykit.utils.time import tz
@@ -17,7 +19,7 @@ def _build_data_path(base_path: path_t, participant_id: str) -> Path:
 
 
 def _build_tabular_data_path(base_path: path_t) -> Path:
-    data_path = base_path.joinpath(f"data_tabular")
+    data_path = base_path.joinpath("data_tabular")
     assert data_path.exists()
     return data_path
 
@@ -34,4 +36,43 @@ def _load_general_information(base_path: path_t, column: str) -> DataFrame:
     df = pd.read_excel(file_path, index_col=0)
     return df[column]
 
+def _build_datetime_path(base_path: path_t, participant_id: str) -> Path:
+    date_dir_path = _build_data_path(base_path, participant_id=participant_id).joinpath(
+        "protocol/cleaned"
+    )
+    date_file_path = date_dir_path.joinpath(f"{participant_id}_protocol.xlsx")
+    assert date_file_path.exists()
+    return date_file_path
 
+def _load_single_date(base_path: path_t, subject_id: str) -> pd.Timestamp:
+    data_path =_build_datetime_path(base_path=base_path, participant_id=subject_id)
+    df = pd.read_excel(data_path, sheet_name="Allgemein", header=None)
+    # Convert the cell to DataFrame indices (C3)
+    cell_value = df.iloc[2, 2]
+    return pd.to_datetime(cell_value, dayfirst=True)
+
+def _update_dates(base_path: path_t, subject_date_dict: dict, sheet_name: str = "Sheet1"):
+    # Load the workbook and select the specified sheet
+    file_path = _build_general_tabular_path(base_path)
+    workbook = load_workbook(file_path)
+    sheet = workbook[sheet_name]
+    sheet["H1"] = "date"
+
+    # Loop through the names and update dates
+    for subject, date in subject_date_dict.items():
+        # Ensure the date is in the desired format
+        if isinstance(date, pd.Timestamp):
+            formatted_date = date.strftime("%d.%m.%Y")
+        else:
+            raise TypeError("Date should be a pd.Timestamp object")
+
+        # Find the row with the matching name
+        for row in sheet.iter_rows(min_row=2, max_row=None, min_col=1, max_col=1):
+            cell = row[0]
+            if cell.value == subject:
+                # Write the date to the corresponding cell in column H
+                sheet[f"H{cell.row}"] = formatted_date
+                break
+
+    # Save the changes to the workbook
+    workbook.save(file_path)
