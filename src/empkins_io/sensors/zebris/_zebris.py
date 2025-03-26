256ae04e125d61331e5327a47990f7ea979cdd26
Joana D. J. <joana.joanidopoulos@fau.de>
2025-03-26 11:01:06 +0100

    WIP _zebris.py

--- src/empkins_io/sensors/zebris/_zebris.py
+++ src/empkins_io/sensors/zebris/_zebris.py
@@ -5,6 +5,38 @@ import pandas as pd
 
 
 class ZebrisDataset:
+    """_sensor_dict = {
+        "force curve": ["values"],  # force-curve.csv
+        "force curve L": ["values"],  # force-curve-L.csv
+        "force curve R": ["values"],  # force-curve-R.csv
+        "force forefoot L %": ["values"],  # parameters.csv
+        "force forefoot R %": ["values"],  # parameters.csv
+        "force backfoot L %": ["values"],  # parameters.csv
+        "force backfoot R %": ["values"],  # parameters.csv
+        "total force %": ["values"],  # parameters.csv
+        "total force R %": ["values"],  # parameters.csv
+        "total force L %": ["values"],  # parameters.csv
+        "force curve backfoot L": ["values"],  # force-curve_backfoot-L.csv
+        "force curve backfoot R": ["values"],  # force-curve_backfoot-R.csv
+        "force curve forefoot L": ["values"],  # force-curve_forefoot-L.csv
+        "force curve forefoot R": ["values"],  # force-curve_forefoot-R.csv
+        "gait line": ["x", "y"],  # gait-line.csv
+        "gait line L": ["x", "y"],  # gait-line-L.csv
+        "gait line R": ["x", "y"],  # gait-line-R.csv
+        "COP": ["x", "y"],  # parameters.csv in [mm]
+        "COP L": ["x", "y"],  # parameters.csv in [mm]
+        "COP R": ["x", "y"],  # parameters.csv in [mm]
+        "COP forefoot L": ["x", "y"],  # parameters.csv [mm]
+        "COP forefoot R": ["x", "y"],  # parameters.csv [mm]
+        "COP backfoot L": ["x", "y"],  # parameters.csv [mm]
+        "COP backfoot R": ["x", "y"],  # parameters.csv [mm]
+        "COP trajectory length": ["values"],  # parameters.csv in [mm]
+        "total time": ["values"],  # parameters.csv
+        "Area of the 95% confidence ellipse": ["values"],  # parameters.csv [mm2]
+        "average velocity": ["values"],  # parameters.csv [mm/s]
+    }
+    """
+
     def __init__(self, path: Union[str, Path], explain: bool = True):
         """
         Initialize ZebrisDataset with a path to a folder or specific CSV file.
