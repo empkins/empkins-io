2dd85364c30cd16579a7454e8883b41eadbbde41
Ronja Michel <ronja.michel@fau.de>
2025-06-29 22:07:17 +0200

    IP: Fix Mocap Loading

--- src/empkins_io/datasets/d07/pilot_study/_dataset.py
+++ src/empkins_io/datasets/d07/pilot_study/_dataset.py
@@ -1,6 +1,7 @@
 from collections.abc import Sequence
 from functools import cached_property, lru_cache
 from itertools import product
+from pathlib import Path
 from typing import ClassVar
 
 import pandas as pd
@@ -101,32 +102,23 @@ class D07PilotStudyDataset(Dataset):
     def mocap(self):
         if not self.is_single(None):
             raise ValueError("Motion capture data can only be accessed for a single participant, condition and phase!")
-
-        # p_id = self.index["participant"][0]
-        # condition = self.index["condition"][0]
-        # phase = self.index["phase"][0]
         p_id = self.group_label.participant
         condition = self.group_label.condition
         phase = self.group_label.phase
 
         # TODO continue
-        data = self._get_mocap_data(p_id)
-        file_path = self.base_path.joinpath(
-            f"data_per_participant/{p_id}/mocap/export/D07_VP_DryRun_Monica_GERTfirst-001.mvnx"
-        )
-
-        # data_slice = data.loc[self.timelog[phase]["start"].iloc[0] : self.timelog[phase]["end"].iloc[0]]
-        # return data_slice
+        file_path = self.base_path.joinpath(f"data_per_participant/{p_id}/mocap/processed/VP_99-001.mvnx")
+        data = self._get_mocap_data(file_path)
 
         # TODO: cut to selected phase by timelog
         timelog = self.timelog.iloc[0]
         data = data.loc[timelog[phase]["start"] : timelog[phase]["end"]]
         return data
 
-    def _get_mocap_data(self, p_id: str) -> pd.DataFrame:
+    def _get_mocap_data(self, file_path: Path) -> pd.DataFrame:
         if self.use_cache:
-            return _cached_load_mocap_data(self.base_path, p_id)
-        return _load_mocap_data(self.base_path, p_id)
+            return _cached_load_mocap_data(file_path)
+        return _load_mocap_data(file_path)
 
     @property
     def condition_order(self):
--- src/empkins_io/datasets/d07/pilot_study/helper.py
+++ src/empkins_io/datasets/d07/pilot_study/helper.py
@@ -1,12 +1,16 @@
+from pathlib import Path
 
-from pandas import DataFrame
+import pandas as pd
 
 from empkins_io.sensors.motion_capture.xsens import XSensDataset
-from empkins_io.utils._types import path_t
 
 
-def _load_mocap_data(base_path: path_t, p_id: str) -> DataFrame:
-    file_path = base_path.joinpath(f"data_per_participant/{p_id}/mocap/processed/{p_id}-002.mvnx")
+def _load_mocap_data(
+    file_path: Path,
+) -> pd.DataFrame:
+    """Load Xsens data for a specific participant."""
+
     dataset = XSensDataset.from_mvnx_file(file_path, tz="Europe/Berlin")
-    return dataset.data_as_df(index="local_datetime")
+    data = dataset.data_as_df(index="local_datetime")
 
+    return data
